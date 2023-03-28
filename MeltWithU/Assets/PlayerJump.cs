a13604ee224841cfaab08e6d81b2d43ac76cd365
roanye <Roan.Yeh@tufts.edu>
2023-03-28 16:57:47 -0400

    charAnimations

--- MeltWithU/Assets/PlayerJump.cs
+++ MeltWithU/Assets/PlayerJump.cs
@@ -6,7 +6,7 @@ public class PlayerJump : MonoBehaviour {
 
       //public Animator anim;
       public Rigidbody2D rb;
-      public float jumpForce = 20f;
+      public float jumpForce = 5f;
       public Transform feet;
       public LayerMask groundLayer;
       public LayerMask enemyLayer;
