dbcbe08dc8d999e6ca76a7a8e0230898463571d5
roanye <Roan.Yeh@tufts.edu>
2023-04-20 12:29:29 -0400

    Rolling: more level design work/mechanics

--- MeltWithU/Assets/Rolling/EnemyCollide.cs
+++ MeltWithU/Assets/Rolling/EnemyCollide.cs
@@ -3,18 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class EnemyCollide : MonoBehaviour
 {
-    public LayerMask enemyLayer;
-    
-    private void OnCollisionEnter(Collision other)
+    public GameObject deadMessage;
+    public bool isDead = false;
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("hit");
-        if (other.gameObject.layer == enemyLayer)
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "enemyRoll")
         {
-            Debug.Log("dead");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            // Stop the player from moving
+            isDead = true;
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            // Set isAlive to false
+            gameObject.GetComponent<PlayerRollJump>().isAlive = false;
+            // Show the dead message
+            deadMessage.SetActive(true);
+
         }
+    }
+
+    void Update() {
+        if (isDead && Input.GetKeyDown(KeyCode.Space)) {
+            RestartScene();
+        }
+    }
+
+    public void RestartScene() {
+        isDead = false;
+        gameObject.GetComponent<PlayerRollJump>().isAlive = true;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
