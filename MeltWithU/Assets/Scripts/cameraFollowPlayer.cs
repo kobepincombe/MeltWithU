f01675c83bc3e73f7c9bb25e5a47dd9cf5c1c152
annaquainliu <annaquainliu@gmail.com>
2023-04-25 13:51:45 -0400

    editing kitchen scene

---
+++ MeltWithU/Assets/Scripts/cameraFollowPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollowPlayer : MonoBehaviour
+{
+    public Transform playerT;
+    private Transform cameraT;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraT = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraT.position = new Vector3 (playerT.position.x, playerT.position.y, playerT.position.z - 50);
+    }
+}
