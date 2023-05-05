b6825a2c4b72451893204aa7b7116dd8db1ecbde
annaquainliu <annaquainliu@gmail.com>
2023-05-05 16:26:28 -0400

    added comments to player script

--- MeltWithU/Assets/Scripts/player.cs
+++ MeltWithU/Assets/Scripts/player.cs
@@ -16,7 +16,7 @@ public class player : MonoBehaviour
     private bool SkeyWasPressed;
     private Transform playerT;
     private BoxCollider2D playerCollider;
-    private bool passThrough;
+    private bool passThrough; //a boolean that records if the player is falling through a platform
 
     // Start is called before the first frame update
     void Start()
@@ -68,6 +68,7 @@ public class player : MonoBehaviour
         rigidbodycomponent.velocity = new Vector2(horizontalinput * 5, rigidbodycomponent.velocity.y);
 
         // if the player's collider is turned off and the player is falling from jumping, then turn player collider on
+        // passthrogh : a boolean that records if the player is falling through a platform
         if ((!passThrough && rigidbodycomponent.velocity.y <= 0.1f) || (passThrough && rigidbodycomponent.velocity.y < -6.5f) 
             && !playerCollider.enabled) {
 
